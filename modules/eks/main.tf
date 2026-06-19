b731eced1ee8953a4073b4f1776c16189322360a
vaibhav <vkolekar217@gmail.com>
2026-06-19 12:37:55 +0530

    increase the k8s version

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -3,27 +3,15 @@ module "eks" {
   version = "~> 20.15"
 
   cluster_name    = var.cluster_name
-  cluster_version = "1.30"
+  cluster_version = "1.35"
 
   subnet_ids = var.private_subnet_ids
   vpc_id     = var.vpc_id
 
   cluster_endpoint_public_access = true
+  
   enable_cluster_creator_admin_permissions = true
-  access_entries = {
-  root_admin = {
-    principal_arn = "arn:aws:iam::892748149955:root"
 
-    policy_associations = {
-      admin = {
-        policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-        access_scope = {
-          type = "cluster"
-        }
-      }
-    }
-  }
-}
   eks_managed_node_groups = {
     default = {
       instance_types = var.instance_types
