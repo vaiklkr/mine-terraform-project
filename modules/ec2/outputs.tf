304781a8ae5908491de2ae111640227037ce4af7
vaibhav <vkolekar217@gmail.com>
2026-06-22 09:31:24 +0530

    remove this keywork from outputs.tf

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -1,9 +1,9 @@
 output "instance_id" {
-  value = aws_instance.this.id
+  value = aws_instance.id
 }
 
 output "public_ip" {
-  value = aws_instance.this.public_ip
+  value = aws_instance.public_ip
 }
 
 output "security_group_id" {
